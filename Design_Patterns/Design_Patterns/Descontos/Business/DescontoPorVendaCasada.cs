5b78fbeb1ac36405dbd6a428faa7e99e00fcff61
Elcio Curanishi <Elcio.Curanishi@gmail.com>
2020-08-15 22:49:11 -0300

    Chain of Responsability - Desconto por venda casada

---
+++ Design_Patterns/Design_Patterns/Descontos/Business/DescontoPorVendaCasada.cs
@@ -0,0 +1,23 @@
+﻿using Design_Patterns.Descontos.Interface;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Design_Patterns.Descontos.Business
+{
+    public class DescontoPorVendaCasada : IDesconto
+    {
+        public IDesconto Proximo { get; set; }
+
+        public double Desconto(Orcamento orcamento)
+        {
+            List<Item> listaItens = (List<Item>)orcamento.Itens;
+
+            if (listaItens.Exists(x => x.Nome.ToUpper() == "LAPIS")
+                && listaItens.Exists(x => x.Nome.ToUpper() == "CANETA"))
+                return orcamento.Valor * 0.05;
+            else
+                return Proximo.Desconto(orcamento);
+        }
+    }
+}
--- Design_Patterns/Design_Patterns/Program.cs
+++ Design_Patterns/Design_Patterns/Program.cs
@@ -6,13 +6,7 @@ namespace Design_Patterns
     class Program
     {
         static void Main(string[] args)
-        {
-            new ExemploIcmsIss();
-            Console.WriteLine();
-            new ExemploIccc();
-            Console.WriteLine();
-            new ExemploInvestimento();
-            Console.WriteLine();
+        {  
             new ExemploDesconto();
         }
     }
